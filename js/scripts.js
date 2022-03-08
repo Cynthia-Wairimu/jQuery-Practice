c552c1298477b582c7de3085cf710d83efdbe0ae
CynthiaWairimu <wairimucee@gmail.com>
2022-03-08 15:28:20 +0300

    add .toggle() methos to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,11 @@ $(document).ready(function() {
     });
   
     $("p").click(function() {
-      alert("This is a paragraph.");
-    });
-  
+        $(".walrus-showing").toggle();
+        $(".walrus-hidden").toggle();
+      });
+    
     $("img").click(function() {
       alert("This is an image.");
     });
-  });
+});
